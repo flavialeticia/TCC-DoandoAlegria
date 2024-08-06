93498a6ef52d16c1d1e35b1c61d39d9d975fe277
Flavia <133375960+flavialeticia@users.noreply.github.com>
2024-08-06 14:32:20 +0000

    miau

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, flash, redirect, request, url_for
 app = Flask(__name__)
 
 @app.route('/')
@@ -20,6 +20,14 @@ def verongs():
 @app.route('/contato')
 def contato():
     return render_template('contato.html')
+
+@app.route('/cadastro', methods=['GET', 'POST'])
+def cadastro():
+    return render_template("cadastro.html")
+
+
+
+
 
 
 
